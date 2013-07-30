b6eb039e21fe91213ec9f0ccadb8060186c3d6cc
BogdanS <BogdanS@baneling.ncct.uottawa.ca>
2013-07-30 16:41:35 -0400

    Uses new JSON library.

--- src/main/java/ca/bsolomon/gw2events/worldevent/DataRetrieveJob.java
+++ src/main/java/ca/bsolomon/gw2events/worldevent/DataRetrieveJob.java
@@ -2,13 +2,11 @@ package ca.bsolomon.gw2events.worldevent;
 
 import java.util.List;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
 import org.joda.time.Chronology;
 import org.joda.time.DateTime;
 
 import ca.bsolomon.gw2event.api.GW2EventsAPI;
+import ca.bsolomon.gw2event.api.dao.Event;
 import ca.bsolomon.gw2events.worldevent.util.EventData;
 
 public class DataRetrieveJob {
@@ -17,17 +15,17 @@ public class DataRetrieveJob {
 	
 	protected boolean queryEvent(Chronology gregorianJuian,
 			List<String> serverIds, String eventUID, EventData dataStructure) {
-		JSONArray data = api.queryServer(eventUID);
+		List<Event> data = api.queryServer(eventUID);
 		boolean changed = false;
 		
 		if (data != null) {
 			for (int i=0; i<data.size(); i++) {
-				JSONObject obj = data.getJSONObject(i);
+				Event obj = data.get(i);
 				
-				String serverId = obj.getString("world_id");
+				String serverId = obj.getWorldId();
 				
 				if (serverIds.contains(serverId)) {
-					String status = obj.getString("state");
+					String status = obj.getState();
 					if (dataStructure.addEventStatus(serverId, eventUID, status, new DateTime(gregorianJuian))) {
 						changed = true;
 					}
